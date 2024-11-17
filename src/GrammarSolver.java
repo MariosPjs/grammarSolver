45cb0fe9419d37b61c9b6d3e897cca779af9122e
Mario Martinez <mariogm@cs.washington.edu>
2024-11-16 23:36:24 -0800

    Initial commit

---
+++ src/GrammarSolver.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class GrammarSolver {
+    private SortedMap<>
+
+    public GrammarSolver(List<String> grammar) {
+        for (String s : grammar) {
+            String
+        }
+
+    }
+}
