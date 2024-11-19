50e69077b70669b58cd4525d8f0293f84ec33997
MariosPjs <131229616+MariosPjs@users.noreply.github.com>
2024-11-18 21:24:12 -0800

    added new methods and need verify from ta(anthony;))

--- src/GrammarSolver.java
+++ src/GrammarSolver.java
@@ -1,5 +1,7 @@
 import java.util.*;
-
+/*
+The grammar solver is a class that utilizes
+ */
 public class GrammarSolver {
     private SortedMap<String, String[]> map = new TreeMap<>();
 
@@ -34,6 +36,7 @@ public class GrammarSolver {
         for(int i = 0; i < times; i++){
             result[i] = generateSentence(symbol);
         }
+        System.out.println(Arrays.toString(result));
         return result;
     }
     public String getSymbols(){
@@ -42,18 +45,28 @@ public class GrammarSolver {
     private String generateSentence(String symbol){
         String sentence = "";
         Random rand = new Random();
+        System.out.println("Symbol: " + symbol);
+
         String[] value = map.get(symbol);
+        System.out.println("current symbol array: " + Arrays.toString(value));
         String rule = value[rand.nextInt(value.length)];
-        String[] check = rule.split("[ \t]+");
+        rule = rule.trim();
+        System.out.println("rule that was picked: " + rule);
+        String[] check = rule.split("\\(|\\)|-|\\s+");
+        System.out.println("check array: " + Arrays.toString(check));
         String word = check[rand.nextInt(check.length)];
+        System.out.println("word that was picked: " + word);
 
-        if(map.containsKey(check[0])){
-            for(int i = 0; i < check.length; i++){
-                sentence = sentence + " " + generateSentence(check[i]);
+
+        if(map.containsKey(word)){
+            System.out.println("    Picking: " + (check[0]));
+
+            sentence = sentence  + generateSentence(check[0]);
+            for(int i = 1; i < check.length; i++){
+                sentence = sentence + " " + generateSentence(check[i]) ;
             }
+            System.out.println(sentence);
             return sentence;
-
-
         }
             return word;
     }
